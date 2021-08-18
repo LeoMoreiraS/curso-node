fde69852656699ce681c78c17f07a7befbdeb10a
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-18 19:47:24 -0300

    feat: add try catch to handle errors in SingUpController

--- src/presentation/controllers/SingUpController.ts
+++ src/presentation/controllers/SingUpController.ts
@@ -4,6 +4,7 @@ import { HttpResponse, HttpRequest } from '../protocols/http';
 import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protocols/Controller';
 import { EmailValidator } from '../protocols/EmailValidator';
+import { ServerError } from '../Errors/ServerError';
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator;
   constructor (emailValidator: EmailValidator) {
@@ -11,12 +12,19 @@ export class SingUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirm'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirm'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
+      }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) { return badRequest(new InvalidParamError('email')); }
+      return badRequest(new InvalidParamError('nope'));
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      };
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) { return badRequest(new InvalidParamError('email')); }
-    return badRequest(new InvalidParamError('nope'));
   }
 }
