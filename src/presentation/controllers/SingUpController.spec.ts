bd11a7bd0f4aaa67f6dd7303b3ff4f7989cbd0a4
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-18 17:28:45 -0300

    test: add test singup controller

---
+++ src/presentation/controllers/SingUpController.spec.ts
@@ -0,0 +1,17 @@
+import { SingUpController } from './SingUpController';
+
+describe('SingUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SingUpController();
+    const httpRequest = {
+      body: {
+        // name: 'Pedrin',
+        email: 'pedrin@gmail.com',
+        password: 'password',
+        passwordConfirm: 'password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
