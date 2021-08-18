f25f6d4204ea72e63df308a0382d09dcf2da9f0f
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-18 17:41:48 -0300

    test: ensure SingupController returns 400 if no emails is provided

--- src/presentation/controllers/SingUpController.spec.ts
+++ src/presentation/controllers/SingUpController.spec.ts
@@ -15,4 +15,18 @@ describe('SingUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SingUpController();
+    const httpRequest = {
+      body: {
+        name: 'Pedrin',
+        // email: 'pedrin@gmail.com',
+        password: 'password',
+        passwordConfirm: 'password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  });
 });
