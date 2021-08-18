58930445183b8f539d2440cbb8b1d6e16af4ff55
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-18 17:58:11 -0300

    refactor: add customized Error for MissingParam

--- src/presentation/controllers/SingUpController.ts
+++ src/presentation/controllers/SingUpController.ts
@@ -1,9 +1,12 @@
+import { MissingParamError } from '../Errors/MissingParamError';
+import { HttpResponse, HttpRequest } from '../protocols/http';
 export class SingUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new Error('Missing param: name') };
+      return { statusCode: 400, body: new MissingParamError('name') };
     } if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new Error('Missing param: email') };
+      return { statusCode: 400, body: new MissingParamError('email') };
     }
+    return { statusCode: 400, body: { oi: 'oi' } };
   }
 }
