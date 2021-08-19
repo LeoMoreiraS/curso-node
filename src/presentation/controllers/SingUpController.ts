a020d82d9c938987d26e237179d28ee7f3909e0e
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-19 16:03:46 -0300

    feat: ensure SingUpController return 400 if passwordConfirm fails

--- src/presentation/controllers/SingUpController.ts
+++ src/presentation/controllers/SingUpController.ts
@@ -13,6 +13,7 @@ export class SingUpController implements Controller {
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirm) { return badRequest(new InvalidParamError('passwordConfirm')); };
       const isValid = this.emailValidator.isValid(httpRequest.body.email);
       if (!isValid) { return badRequest(new InvalidParamError('email')); }
       return badRequest(new InvalidParamError('nope'));
