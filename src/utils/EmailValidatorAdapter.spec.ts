594ad5684f459d01c465645007e01a5cfd9bd78d
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-21 00:32:47 -0300

    refactor: move sut creation to a factory method

--- src/utils/EmailValidatorAdapter.spec.ts
+++ src/utils/EmailValidatorAdapter.spec.ts
@@ -6,20 +6,23 @@ jest.mock('validator', () => ({
     return true;
   }
 }));
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
 describe('EmailValidator Adapter', () => {
   test('should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid-email@gmail.com');
     expect(isValid).toBe(false);
   });
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid('valid-email@gmail.com');
     expect(isValid).toBe(true);
   });
   test('should call validator with correct email address', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, 'isEmail');
     sut.isValid('any-email@gmail.com');
     expect(isEmailSpy).toHaveBeenCalledWith('any-email@gmail.com');
